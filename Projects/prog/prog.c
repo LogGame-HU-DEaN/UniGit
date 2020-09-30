fb2cd92dd47c11e960d450ab9ffea49d7a6de594
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-09-30 14:53:04 +0200

    4

---
+++ Projects/prog/prog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float szazalekErtek(float a, float l) {
+  float szertek;
+  szertek = a * l / 100;
+  return szertek;
+}
+
+int main(int argc, char const *argv[]) {
+  float ertek, lab, alap;
+
+  printf("alap: ");
+  scanf("%f", &alap);
+  printf("lab: ");
+  scanf("%f", &lab);
+
+  ertek = szazalekErtek(alap, lab);
+
+  printf("%f %f szazaleka = %f\n", alap, lab, ertek);
+  return 0;
+}
