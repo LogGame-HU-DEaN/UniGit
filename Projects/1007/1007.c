07c5012bb6555332cd88170a6ce3bf513819e62e
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-10-07 14:48:24 +0200

    commit

--- Projects/1007/1007.c
+++ Projects/1007/1007.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fillArray(int *numArray, int size) {
+  size = 5;
+
+  for (int i = 0; i < size; i++) {
+    printf("%d. value: ", i + 1);
+    scanf("%d", &numArray[i]);
+  }
+
+  printf("\nArray values:\n\n");
+
+  for (int i = 0; i < size; i++) {
+    printf("%d. value: %d\n", i + 1, numArray[i]);
+  }
+
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = i + 1; j < size; j++) {
+      if (numArray[j] < numArray[i]) {
+        int sw = numArray[j];
+        numArray[j] = numArray[i];
+        numArray[i] = sw;
+      }
+    }
+  }
+
+  printf("\nOrdered array:\n\n");
+
+  for (int i = 0; i < size; i++) {
+    printf("%d. value: %d\n", i + 1, numArray[i]);
+  }
+}
+
+/*int fill(const void *a, const void *b) {
+  if (*(int *)a == *(int *)b)
+    return 0;
+  return *(int *)a < *(int *)b ? -1 : 1;
+}*/
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  int size = 5, numArray[size];
+
+  fillArray(numArray, size);
+  /*qsort(numArray, size, sizeof(int), fill);*/
+
   return 0;
 }
