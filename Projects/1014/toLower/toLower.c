1a7c5c0cf375afcbbf096a3e4babe18a64625073
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-10-14 15:44:20 +0200

    commit

--- Projects/1014/toLower/toLower.c
+++ Projects/1014/toLower/toLower.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void changeUpLow(char *string) {
+  int i = 0;
+  while (string[i] != 0) {
+    if (string[i] >= 'A' && string[i] <= 'Z') {
+      string[i] += ' ';
+    } else if (string[i] >= 'a' && string[i] <= 'z') {
+      string[i] -= ' ';
+    }
+    i++;
+  }
+}
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  char string[1];
+  scanf("%[^\n]", string);
+  printf("Eredeti szoveg: %s\n", string);
+  changeUpLow(string);
+  printf("Forditott szoveg: %s\n", string);
   return 0;
 }
