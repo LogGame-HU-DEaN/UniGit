79d8a34dbc5ed5fd04d58f7d450bf1e97a30130b
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-11-04 14:35:51 +0100

    commit

--- Projects/1104/1104.c
+++ Projects/1104/1104.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  int i, n = 10;
+  FILE *fp = NULL;
+  fp = fopen("szamFajl", "w");
+  for (i = 1; i <= n; i++) {
+    fprintf(fp, "%d\n", i);
+  }
+  fflush(fp);
+  fclose(fp);
   return 0;
 }
