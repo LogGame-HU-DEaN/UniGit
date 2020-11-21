40828e1719bd806b22e2caffbeb6a24dfbc0dae7
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-11-21 10:35:33 +0100

    beadando commit

--- Projects/beadando/beadando.c
+++ Projects/beadando/beadando.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct donor {
+  int id;
+  char name[20];
+  char btype[3];
+  char email;
+  int times;
+  ldate();
+} donor;
+
 int main(int argc, char const *argv[]) {
   /* code */
   return 0;
