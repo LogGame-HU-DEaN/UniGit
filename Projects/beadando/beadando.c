0091fcc53c28d1324d163b6f53e0eba28118c9bd
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2020-11-21 11:07:53 +0100

    commit

--- Projects/beadando/beadando.c
+++ Projects/beadando/beadando.c
@@ -12,8 +12,13 @@ typedef struct donor {
 
 char ldate(char *ldate[]) {
   for (int i = 0; *ldate[i] != '\0'; ++i) {
-    while (i == 5 || i == 8 || i == 11) {
+    if (i != 5 && i != 8 && i != 11) {
+      if (*ldate[i] < '0' && *ldate[i] > '9') {
+        return printf("Hibas datum!\n");
+      }
+    } else if (i == 5 || i == 8 || i == 11) {
       if (*ldate[i] != '.') {
+        return printf("Hibas datum formatum!\n");
       }
     }
   }
